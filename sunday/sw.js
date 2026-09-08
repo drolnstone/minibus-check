507a871eee4c286546c31055bbe5fb1fb6fc0cd7
Tata_Asim <asimbassey@yahoo.com>
2026-09-08 21:07:14 +0100

    Update service worker cache version to v1.44.0

--- sunday/sw.js
+++ sunday/sw.js
@@ -18,7 +18,7 @@
    BUMP CACHE below after editing index.html in this folder, or phones keep
    the old copy. */
 const CACHE_PREFIX = "minibus-sunday-";
-const CACHE = CACHE_PREFIX + "v1.43.3";
+const CACHE = CACHE_PREFIX + "v1.44.0";
 
 /* What a passenger needs to see a page at all.
 
